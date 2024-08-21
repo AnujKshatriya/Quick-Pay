f939cfd07b42f58780bc25ee896ce01e18c8f213
AnujKshatriya <anujkshatriyaiitian@gmail.com>
2024-08-21 19:45:13 +0530

    font family changed

--- apps/merchant-app/app/layout.tsx
+++ apps/merchant-app/app/layout.tsx
@@ -1,9 +1,6 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import { Providers } from "../provider";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Turborepo",
@@ -18,7 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Providers>
-        <body className={inter.className}>{children}</body>
+        <body >{children}</body>
       </Providers>
     </html>
   );
